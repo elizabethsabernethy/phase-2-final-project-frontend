047331222b4f41b3c732f1f4a8c60a708819fcc7
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-04 12:09:57 -0500

    adds onClick function to book-cover div so the user can see the details of the specific book when it is clicked. Also adds state to help show whetehr the details of each book should be hidden or shown.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Book({book}){
 
+    const[hideDetails, setHideDetails] = useState(true)
+
+    function clickBook(){
+        setHideDetails((hideDetails) => !hideDetails)
+    }
+
     return(
         <div>
-            <h2>{book.title} - {book.year}</h2>
-            <h3>{book.author}</h3>
-            <p>{book.summary}</p>
-            <ul>
-                {book.themes.map((theme)=>{
-                    return <li key={theme}>{theme}</li>
-                })}
-            </ul>
-            <h4>Likes: {book.likes}</h4>
-            <button>Like Book</button>  <button>Unlike Book</button>
+            <div className="book-cover" onClick={clickBook}>
+                <h2>{book.title} - {book.year}</h2>
+                <h3>{book.author}</h3>
+            </div>
+            <div className="open-book" hidden={hideDetails}>
+                <p>{book.summary}</p>
+                <ul>
+                    {book.themes.map((theme)=>{
+                        return <li key={theme}>{theme}</li>
+                    })}
+                </ul>
+                <h4>Likes: {book.likes}</h4>
+                <button>Like Book</button>  <button>Unlike Book</button> 
+            </div>
+           
         </div>
     )
 }
