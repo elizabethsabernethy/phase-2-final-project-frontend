45f5ef4b6c4cfa467fd2b54b0743f25ae98ba0c0
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-04 13:01:40 -0500

    adds hidden attribute to book-cover and onclick function to open-book so the user can toggle between seeing the title,author, year and the book details

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,12 +10,12 @@ function Book({book}){
 
     return(
         <div className="book">
-            <div className="book-cover" onClick={clickBook}>
+            <div className="book-cover" hidden={!hideDetails} onClick={clickBook}>
                 <h2 className="title">{book.title}</h2>
                 <h3 className="author">{book.author}</h3>
                 <h3 className="year">{book.year}</h3>
             </div>
-            <div className="open-book" hidden={hideDetails}>
+            <div className="open-book"  onClick={clickBook} hidden={hideDetails}>
                 <p className="summary">{book.summary}</p>
                 <ul>
                     {book.themes.map((theme)=>{
