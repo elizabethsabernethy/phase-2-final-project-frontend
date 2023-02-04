dd7325ff136fbdaffe060618c0550b48d018e347
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-04 13:44:49 -0500

    adds NavBar file. Inserted initial home navlink and required imports/ export

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+function NavBar(){
+
+    const linkStyles = {
+        display: "inline-block",
+        width: "70px",
+        padding: "12px",
+        margin: "0 6px 6px",
+        background: "black",
+        textDecoration: "none",
+        color: "white",
+      };
+
+    return(
+        <div className="navbar">
+            <NavLink
+            to="/"
+            exact
+            style={linkStyles}
+            activeStyle={{
+                background:'red'
+            }}
+            >
+                Home
+            </NavLink>
+        </div>
+    )
+}
+
+export default NavBar;
