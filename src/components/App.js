71d0737372f32880c89c50678c12efd518307e7e
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-04 13:59:45 -0500

    installs react router dom @5. Updates App and index.js to properly import react router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import BookList from "./BookList";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App(){
   return(
     <div>
-      <BookList />
+    <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <BookList />
+        </Route>
+      </Switch>
     </div>
   )
 }
