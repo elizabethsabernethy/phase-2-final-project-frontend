642cd09364d1d536ddaaffc77b6daa1a49389f93
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-09 09:29:08 -0500

    moves likes outside of open cover to below book cover inside own likes-div

--- src/components/Book.js
+++ src/components/Book.js
@@ -13,7 +13,11 @@ function Book({book}){
             <div className="book-cover" hidden={!hideDetails} onClick={clickBook}>
                 <h2 className="title">{book.title}</h2>
                 <h3 className="author">{book.author}</h3>
-                <h3 className="year">{book.year}</h3>
+                <h3 className="year">{book.year}</h3> 
+            </div>
+            <div className="likes-div">
+                <h4 className="likes">Likes: {book.likes}</h4>
+                <button>Like Book</button>  <button>Unlike Book</button>
             </div>
             <div className="open-book"  onClick={clickBook} hidden={hideDetails}>
                 <p className="summary">{book.summary}</p>
@@ -22,8 +26,7 @@ function Book({book}){
                         return <li className="theme" key={theme}>{theme}</li>
                     })}
                 </ul>
-                <h4 className="likes">Likes: {book.likes}</h4>
-                <button>Like Book</button>  <button>Unlike Book</button> 
+                
             </div>
            
         </div>
