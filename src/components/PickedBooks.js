126e4320bb139e69362a06150dbb413c159f84ed
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-09 13:07:17 -0500

    adds basic import, function and export

---
+++ src/components/PickedBooks.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Book from "./Book";
+
+function PickedBooks(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default PickedBooks;
