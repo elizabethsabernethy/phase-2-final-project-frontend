c9f08273c3134bd01b9f31426f2382b29ab92251
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-09 09:37:20 -0500

    moves likes-div to above book cover div so when user clicks book cover, likes div won't jump above open book

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,14 +10,14 @@ function Book({book}){
 
     return(
         <div className="book">
+            <div className="likes-div">
+                <h4 className="likes">Likes: {book.likes}</h4>
+                <button>Like Book</button>  <button>Unlike Book</button>
+            </div>
             <div className="book-cover" hidden={!hideDetails} onClick={clickBook}>
                 <h2 className="title">{book.title}</h2>
                 <h3 className="author">{book.author}</h3>
                 <h3 className="year">{book.year}</h3> 
-            </div>
-            <div className="likes-div">
-                <h4 className="likes">Likes: {book.likes}</h4>
-                <button>Like Book</button>  <button>Unlike Book</button>
             </div>
             <div className="open-book"  onClick={clickBook} hidden={hideDetails}>
                 <p className="summary">{book.summary}</p>
