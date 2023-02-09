735aa7fa608d5160310cb8edad8df87570cfa020
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-09 14:49:37 -0500

    adds basic welcome text and info about the site, as well as shows a placeholder for the top 3 books based on likes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,18 @@ import React from "react";
 function Home(){
 
     return(
-        <div>
-            
+        <div id="home">
+            <h1>Welcome to the Library!</h1>
+            <h3>
+                Here at the Library, you can view books in the collection,
+                check books out, and even add new books!
+            </h3>
+            <h4>Top 3 favorite books at the Library</h4>
+            <ol>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+            </ol>
         </div>
     )
 }
