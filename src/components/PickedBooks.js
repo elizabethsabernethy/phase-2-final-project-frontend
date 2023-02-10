f25c42557869c3bd2f6e4581ba4201f1aff53217
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-10 08:33:48 -0500

    adds elements from book to replicate book component once clicked within collectio

--- src/components/PickedBooks.js
+++ src/components/PickedBooks.js
@@ -1,11 +1,30 @@
-import React from "react";
-import Book from "./Book";
+import React, {useState} from "react";
 
-function PickedBooks(clickedBook){
+function PickedBooks({clickedBook}){
+
+    const[hideDetails, setHideDetails] = useState(true)
+
+    function hoverBook(){
+        setHideDetails((hideDetails) => !hideDetails)
+    }
 
     return(
-        <div>
+        <div className="book">
             {console.log(clickedBook)}
+            <div className="book-cover" hidden={!hideDetails} onMouseEnter={hoverBook}>
+                <h2 className="title">{clickedBook.title}</h2>
+                {/* <h3 className="author">{clickedBook.author}</h3>
+                <h3 className="year">{clickedBook.year}</h3>  */}
+            </div>
+            <div className="open-book"  onMouseLeave={hoverBook} hidden={hideDetails}>
+                <p className="summary">{clickedBook.summary}</p>
+                {/* <p className="theme-header">Themes</p>
+                <ul>
+                    {clickedBook.themes.map((theme)=>{
+                        return <li className="theme" key={theme}>{theme}</li>
+                    })}
+                </ul> */}
+            </div>
         </div>
     )
 }
