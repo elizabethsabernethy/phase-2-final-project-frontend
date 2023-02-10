ad660897363a7cd7c6f39495fc1f1393f64ac835
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-10 10:34:43 -0500

    adds post request to persit selected book from collection to my books

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BookList from "./BookList";
 import PickedBooks from "./PickedBooks";
 import { Route, Switch } from "react-router-dom";
@@ -7,18 +7,39 @@ import Home from "./Home";
 
 function App(){
 
-  const[clickedBooks, setClickedBooks] = useState([])
+  const[myBooks, setMyBooks] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/myBooks')
+    .then((resp)=> resp.json())
+    .then((myBooks)=> setMyBooks(myBooks))
+  },[])
 
   function pickBook(book){
-    setClickedBooks([...clickedBooks, book])
-  }
+    fetch('http://localhost:3000/myBooks',{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+      "title" : book.title,
+      "author": book.author,
+      "summary": book.summary,
+      "themes": book.themes,
+      "likes": book.likes,
+      "year": book.year
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((myBook)=> setMyBooks([...myBooks, myBook]))
+    }
 
   return(
     <div>
     <NavBar />
       <Switch>
         <Route exact path="/picked">
-          <PickedBooks clickedBooks={clickedBooks}/>
+          <PickedBooks clickedBooks={myBooks}/>
         </Route>
         <Route exact path="/collection">
           <BookList pickBook={pickBook}/>
