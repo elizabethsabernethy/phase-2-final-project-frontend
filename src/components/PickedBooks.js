30311d302e803f257e6f3cd88e86f1c9a659fdb3
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-10 10:56:54 -0500

    adds click fucntion and callback function prop

--- src/components/PickedBooks.js
+++ src/components/PickedBooks.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function PickedBooks({clickedBooks}){
+function PickedBooks({returnBook, clickedBooks}){
 
     //use fecth to get myBooks?
     //add click function so user can return book, fetch delete from myBooks db.json
@@ -10,12 +10,16 @@ function PickedBooks({clickedBooks}){
     function hoverBook(){
         setHideDetails((hideDetails) => !hideDetails)
     }
+    
+    function clickBook(book){
+        returnBook(book)
+    }
 
     return(
         <div >
             {clickedBooks.map((book)=>{
                 return (
-                    <div className="book" key={book.id}>
+                    <div className="book" key={book.id} onClick={()=>{clickBook(book)}}>
                     <div className="book-cover" hidden={!hideDetails} onMouseEnter={hoverBook}>
                         <h2 className="title">{book.title}</h2>
                         <h3 className="author">{book.author}</h3>
