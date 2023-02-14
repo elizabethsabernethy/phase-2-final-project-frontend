0060766e52c8c08d2477fc08c9375b82dec721ed
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 17:54:48 -0500

    adds filter component and imports it

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Book from "./Book";
+import Filter from "./Filter";
 
 function BookList({books, setBooks, onPickBook, onRemoveBook}){
 
@@ -19,6 +20,7 @@ function BookList({books, setBooks, onPickBook, onRemoveBook}){
           <h2><u>There are</u> {books.length} <u>books available to be checked out</u></h2>
           <h4>To check a book out, simply click on the book</h4>
           <h4>To like a book, hit the heart button below the book</h4>
+          <Filter />
             {books.map((book)=>{
                 return <Book key={book.id} book={book} likeBook={likeBook} onPickBook={onPickBook} onRemoveBook={onRemoveBook}/>
             })}
