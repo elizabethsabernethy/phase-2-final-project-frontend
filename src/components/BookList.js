b97b10cc352bc6bcc475ef4c6d7d15ceda564d6f
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 17:40:16 -0500

    adds info for the user to check a book out, like a book, and shows how many books are in the collection

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -16,6 +16,9 @@ function BookList({books, setBooks, onPickBook, onRemoveBook}){
 
     return(
         <div>
+          <h2><u>There are</u> {books.length} <u>books available to be checked out</u></h2>
+          <h4>To check a book out, simply click on the book</h4>
+          <h4>To like a book, hit the heart button below the book</h4>
             {books.map((book)=>{
                 return <Book key={book.id} book={book} likeBook={likeBook} onPickBook={onPickBook} onRemoveBook={onRemoveBook}/>
             })}
