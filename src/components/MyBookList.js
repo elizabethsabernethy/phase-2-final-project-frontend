422d4be45ba0637a386185868f54b27853914d50
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 17:35:30 -0500

    adds info for the user to understand how to return book as well as a note that shows user how many books they have currently checked out

--- src/components/MyBookList.js
+++ src/components/MyBookList.js
@@ -5,6 +5,8 @@ function MyBookList({onBookReturn, clickedBooks, onRemoveMyBook}){
 
     return(
         <div>
+            <h2><u>You have</u> {clickedBooks.length} <u>books checked out</u></h2>
+            <h4>(To return a book back to the collection, simply click on the book)</h4>
             {clickedBooks.map((book)=>{
                 return <PickedBooks key={book.id} onBookReturn={onBookReturn} book={book} onRemoveMyBook={onRemoveMyBook}/>
             })}
