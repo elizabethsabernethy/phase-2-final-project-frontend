83a4509b574cac79f1fa2cbebb21145400e7b308
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 17:54:21 -0500

    adds label and text input to create search bar

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,10 @@ import React from "react";
 function Filter(){
     return(
         <div>
-         
+            <label>
+                Filter Books by name: 
+                <input type='text'></input>
+            </label>
         </div>
     )
 }
