44f0dadff7648e6a83348be5523aef0070194a00
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 20:59:20 -0500

    adds callback function and props to filter book based on title, as well as adding a filteredbooks state

--- src/components/MyBookList.js
+++ src/components/MyBookList.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PickedBooks from "./PickedBooks";
 
 function MyBookList({onBookReturn, clickedBooks, onRemoveMyBook}){
+
+    const [filteredTitles, setFilteredTitles] = useState('')
+
+    function filterTitles(input){
+        setFilteredTitles(input)
+      }
+      
+      const booksToShow = clickedBooks.filter((book)=>{
+        return ((book.title).toLowerCase()).match(filteredTitles.toLowerCase());
+      })
 
     return(
         <div>
             <h2><u>You have</u> {clickedBooks.length} <u>books checked out</u></h2>
             <h4>(To return a book back to the collection, simply click on the book)</h4>
-            {clickedBooks.map((book)=>{
-                return <PickedBooks key={book.id} onBookReturn={onBookReturn} book={book} onRemoveMyBook={onRemoveMyBook}/>
+            {booksToShow.map((book)=>{
+                return <PickedBooks key={book.id} filterTitles={filterTitles} onBookReturn={onBookReturn} book={book} onRemoveMyBook={onRemoveMyBook}/>
             })}
         </div>
     )
