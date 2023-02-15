91f53c23ae88bbb8bed1acb0d8073f3dcdae0c97
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 21:16:31 -0500

    adds function to handle sort, adds callback prop, and adds selection and options for sorting

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function Sort({}){
+function Sort({handleSetSortBy}){
+
+    function sortBooks(event){
+        handleSetSortBy(event.target.value)
+    }
 
     return(
         <div>
-
+            <select onChange={sortBooks} name="books_Sort">
+            <option value="Sort Books">Sort Books</option>
+            <option value="A-Z">A - Z</option>
+            <option value="Z-A">Z - A</option>
+            <option value="oldest">Oldest</option>
+            <option value="newest">Newest</option>
+            <option value="most-likes">Most Liked</option>
+            </select>
         </div>
     )
 }
