c346a5972dc52c23e7181d90c0c3ec743eaed844
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-02-14 21:18:57 -0500

    changes alphabestized sort to specify titles

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -10,8 +10,8 @@ function Sort({handleSetSortBy}){
         <div>
             <select onChange={sortBooks} name="books_Sort">
             <option value="Sort Books">Sort Books</option>
-            <option value="A-Z">A - Z</option>
-            <option value="Z-A">Z - A</option>
+            <option value="A-Z">Titles: A - Z</option>
+            <option value="Z-A">Titles: Z - A</option>
             <option value="oldest">Oldest</option>
             <option value="newest">Newest</option>
             <option value="most-likes">Most Liked</option>
